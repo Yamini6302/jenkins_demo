5deffa568ef49002fa478b11165f32cc8fe9796e
Yamini6302 <129651557+Yamini6302@users.noreply.github.com>
2024-10-17 15:43:14 +0530

    Delete department-service (5).zip_expanded directory

--- department-service (5).zip_expanded/department-service/src/main/java/com/demo/department_service/dao/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.department_service.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.demo.department_service.dao.entity.DepartmentEntity;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Long>{}
